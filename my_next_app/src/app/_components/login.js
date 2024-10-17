ba93c4ffaf627385eaaab52f3cd43fd8309b96f8
Ritik Mittal <ritik_mittal@Ritiks-MacBook-Air.local>
2024-10-18 00:21:34 +0530

    validation for login page

--- my_next_app/src/app/_components/login.js
+++ my_next_app/src/app/_components/login.js
@@ -7,11 +7,79 @@ function Login() {
         password: ''
     })
 
-    const handleInputChange = (event) => {
+    const [formDataError, setFormDataError] = useState({
+      email: '',
+      password: ''
+    })
+
+    const [isError, setError] = useState(false)
+
+    const validateEmail = (value) => {
+      const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+      return emailRegex.test(value);
+    }
+    const validatePassword = (value) => {
+      return value.length >= 5;
+    }
+
+    const validateField = (value, fieldName) => {
+      switch(fieldName){
+        case 'email':
+          return validateEmail(value)
+        case 'password':
+          return validatePassword(value)
+        default:
+          return true
+      }
+    }
+
+    const handleCustomErrorMessage = (fieldName) => {
+      switch(fieldName){
+        case 'email':
+          return 'email is incorrect'
+        case 'password':
+          return 'min 5 or more characters required'
+        default:
+          return ''
+      }
+    }
+
+    function handleInputChange(event){
+      const {name, value} = event.target
         setLoginData({
             ...loginData,
-            [event.target.name]: event.target.value
+            [name]: value
         })
+
+        const tempError = {...formDataError}
+        if(!value.length){
+          tempError[name] = `${name} is required`
+        } else if (!validateField(value,name)){
+          tempError[name] = handleCustomErrorMessage(name)
+        } else {
+          tempError[name] = ''
+        }
+        setFormDataError(tempError)
+
+        if (Object.values(tempError).some((error) => error !== "")) {
+          setError(true); // If any field has an error, set isError to true
+        } else {
+          setError(false); // No errors, so set isError to false
+        }
+
+    }
+
+    const handleOnFocus = (event) => {
+      const {name, value} = event.target
+      if(!value.length){
+        setFormDataError({
+          ...formDataError,
+          [name]: `${name} is required`
+        })
+        setError(true)
+      } else {
+        setError(false)
+      }
     }
   return (
     <>
@@ -24,7 +92,13 @@ function Login() {
             placeholder="enter your username/email"
             onChange={handleInputChange}
             value={loginData.email}
+            onFocus={handleOnFocus}
           />
+          {formDataError?.email?.length ? (
+            <div style={{ color: "red" }}>{formDataError?.email}</div>
+          ) : (
+            ""
+          )}
         </div>
         <div className="input-wrapper">
           <input
@@ -33,7 +107,13 @@ function Login() {
             placeholder="enter your password"
             onChange={handleInputChange}
             value={loginData.password}
+            onFocus={handleOnFocus}
           />
+           {formDataError?.password?.length ? (
+            <div style={{ color: "red" }}>{formDataError?.password}</div>
+          ) : (
+            ""
+          )}
         </div>
         <div className="input-wrapper">
           <button className="button-wrapper">Login</button>
